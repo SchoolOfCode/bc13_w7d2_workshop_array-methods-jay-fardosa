6fdc8394bc79fddbd0fb540f979214f0ddc27070
diinif <fdiini91@gmail.com>
2022-11-29 14:10:09 +0000

    new variable added to test branching

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+// const capitalisedCats 
+// your code here
+
+
+// New fardosas branch 
+let array = []
+let newArray = []
